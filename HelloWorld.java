fa0b3195eb5c649ff4967c0a3b263400657fbdf9
MJHarding <33782780+MJHarding@users.noreply.github.com>
2018-02-05 11:57:29 +0000

    HelloWorld first java files

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+/* First java file that prints hello world
+ * This is as tradition dictates the first program I will make un Java */
+
+ public class HelloWorld {
+  public static void main(String[] args) {
+   System.out.println("Hello World");
+  }
+ }
